fa757379bd8c95962b196df3cb014c096847d194
Maricy Caregnato <cmaricy@gmail.com>
2016-10-07 21:15:30 -0300

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'facebookAuth' : {
 		'clientID' : '1080743178644933',
-		'clientSecret' : '6e489c281c1928f822a352f7fdfc8368',
+		'clientSecret' : 'xxxxxxxxxxxxxxxxxx',
 		'callbackURL' : 'https://vivamente-1338.appspot.com/auth/facebook/callback'
 	}
 };
